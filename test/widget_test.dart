45356ec6a9a30b57f178cc406f82e4e14ccb259f
Fabbro96 <fabbro96@users.noreply.github.com>
2026-03-14 16:04:26 +0100

    Fix final unused imports from widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,14 @@
 // Basic smoke test for the Solar Power Manager app.
 
-import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:solar_power_manager/controllers/energy_controller.dart';
 import 'package:solar_power_manager/main.dart';
-import 'package:solar_power_manager/services/app_log_service.dart';
 import 'package:solar_power_manager/services/energy_service.dart';
 
 class _NoopEnergyController extends EnergyController {
-  _NoopEnergyController({required super.service, super.logService});
+  _NoopEnergyController({required super.service});
 
   @override
   void start() {}
